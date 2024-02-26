f4b94d7aa006bf24d57743d22c2576b6e6a1d6f6
manu <manu.softwareengineer@gmail.com>
2024-02-26 00:00:00 +0000

    models/

---
+++ models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  history: [
+    {
+      image: {
+        buffer: Buffer,
+        contentType: String,
+      },
+      prediction: {
+        label: String,
+        confidence: Number,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
