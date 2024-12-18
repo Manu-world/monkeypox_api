419733e5f2bc19f91245c58739e7e35519207780
Manu-world <manu.softwareengineer@gmail.com>
2024-12-18 10:55:23 +0000

    fix: package issues

--- app.js
+++ app.js
@@ -19,11 +19,20 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [process.env.CLIENT_URL, process.env.STAGING_URL];
+const allowedOrigins = [process.env.CLIENT_URL, process.env.STAGING_URL, "http://localhost:5173"];
 app.use(
   cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
